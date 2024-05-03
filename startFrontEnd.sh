5f5c5659973caac3db210ea73d1a0fb5b84285e5
Alessandro <schiavo.alessandro@protonmail.com>
2024-05-03 14:23:18 +0200

    Auto created commit at 03_05_2024_14_22

--- startFrontEnd.sh
+++ startFrontEnd.sh
@@ -1,6 +1,6 @@
 echo "inserisci qui i comandi per lo starting del Front-End della tua applciazione web"
 echo "Inizio comandi installazione precondizioni"
-cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale
+cd /home/runner/work/A2-Spotify/A2-Spotify
 #curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
 #cat nodesource_setup.sh
 #sudo bash nodesource_setup.sh
@@ -12,7 +12,7 @@ node -v
 echo "Fine comandi installazione Node"
 
 echo "Inizio comandi esecuzione Frontend"
-cd /home/runner/work/Tesi-StrumentoGenerale/Tesi-StrumentoGenerale/insert-here-your-web-app/angular-spotify-main/angular-spotify-main
+cd /home/runner/work/A2-Spotify/A2-Spotify/insert-here-your-web-app/angular-spotify-main/angular-spotify-main
 echo "Siamo nella directory FE, proviamo a lanciarlo in esecuzione"
 #npm install
 npm ci
